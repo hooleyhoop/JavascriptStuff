c87a29cfcad8ac7f6626e23f735115c25a491718
HooleyHoop <steven.hooley@gmail.com>
2010-10-31 23:36:38 +0000

    i will i will test

---
+++ spec/lib/gui/hoo_view_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "HooView" do
+
+  before(:each) do
+  end
+
+  describe "creation" do
+
+    describe "failure" do
+		it "should fail" do
+    		42.should_be 43
+    	end
+    end
+
+    describe "success" do
+    end
+   
+  end
+end
---
+++ spec/lib/presenters/hoo_presenter_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "HooPresenter" do
+
+  before(:each) do
+  end
+
+  describe "creation" do
+
+    describe "failure" do
+    end
+
+    describe "success" do
+    end
+
+  end
+end
